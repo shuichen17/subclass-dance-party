8f06f63bc3477275a24b7e328ec0b9b1709f93e2
shuichen17 <shuichen17@gmail.com>
2019-04-15 19:49:08 -0700

    Added Blue, Red, Green Dancer functionality.  Updated background

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -1,34 +1,34 @@
-describe('blinkyDancer', function() {
+describe('redDancer', function() {
 
-  var blinkyDancer, clock;
+  var redDancer, clock;
   var timeBetweenSteps = 100;
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = new makeBlinkyDancer(10, 20, timeBetweenSteps);
+    redDancer = new makeRedDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
-    expect(blinkyDancer.$node).to.be.an.instanceof(jQuery);
+    expect(redDancer.$node).to.be.an.instanceof(jQuery);
   });
 
   it('should have a step function that makes its node blink', function() {
-    sinon.spy(blinkyDancer.$node, 'toggle');
-    blinkyDancer.step();
-    expect(blinkyDancer.$node.toggle.called).to.be.true;
+    sinon.spy(redDancer.$node, 'toggle');
+    redDancer.step();
+    expect(redDancer.$node.toggle.called).to.be.true;
   });
 
   describe('dance', function() {
     it('should call step at least once per second', function() {
-      sinon.spy(blinkyDancer, 'step');
-      expect(blinkyDancer.step.callCount).to.be.equal(0);
+      sinon.spy(redDancer, 'step');
+      expect(redDancer.step.callCount).to.be.equal(0);
       clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
       clock.tick(timeBetweenSteps);
 
-      expect(blinkyDancer.step.callCount).to.be.equal(1);
+      expect(redDancer.step.callCount).to.be.equal(1);
 
       clock.tick(timeBetweenSteps);
-      expect(blinkyDancer.step.callCount).to.be.equal(2);
+      expect(redDancer.step.callCount).to.be.equal(2);
     });
   });
 });
